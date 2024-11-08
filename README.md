c926d0f8e670d428dd41f52dbde9232bd180967e
Xiao Mai <ethanmyers1995@gmail.com>
2024-11-08 15:01:47 +0000

    feat: added ssid dependancy for creating short url's

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 - MongoDB to store original url's 
 - dns to verify the URL submitted, returning a json with error if error exists
 - middleware: Express' urlencoded method to parse URL-encoded data, allowing the data to be requested from request.body in the POST request.
+- ssid: An npm package that generates by default a 8-character random string, used here to create shortened URLs. Repo: github.com/rohitnirban/ssid 
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const dns = require('dns');
+const mongoose = require('mongoose');
+const generateShortId= require('ssid')
 const app = express();
 
 //Middleware to parse URL-encoded data, used later in POST request
@@ -17,6 +19,24 @@ app.use('/public', express.static(`${process.cwd()}/public`));
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+//Connect to Mongoose
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Check connection
+mongoose.connection.on('connected', () => {
+  console.log('connected')
+})
+
+mongoose.connection.on('error', (err) => {
+  console.log(err)
+})
+
+mongoose.connection.on('disconnected', () => {
+  console.log('disconnected')
+})
+
+
 
 app.post('/api/shorturl', (req, res) => {
   let url =  new URL(req.body.url);
